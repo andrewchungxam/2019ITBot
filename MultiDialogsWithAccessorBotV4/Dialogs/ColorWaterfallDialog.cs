597403576aa16526eb46b84200bd0da665105191
andrewchungxam <andrew.chung@xamarin.com>
2019-04-02 15:03:07 -0400

    cleaned comments
    cleaned comments

--- MultiDialogsWithAccessorBotV4/Dialogs/ColorWaterfallDialog.cs
+++ MultiDialogsWithAccessorBotV4/Dialogs/ColorWaterfallDialog.cs
@@ -37,7 +37,6 @@ namespace Bot_Builder_Simplified_Echo_Bot_V4
         private static async Task<DialogTurnResult> NameStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
             string stringFromFirstStep = (string)stepContext.Result;
-            //await stepContext.Context.SendActivityAsync(MessageFactory.Text($"COLOR WATERFALL STEP 2: You can pass objects/strings step-to-step like this: {stringFromFirstStep}"), cancellationToken);
             return await stepContext.PromptAsync("colorName", new PromptOptions { Prompt = MessageFactory.Text("What is your company email address?") }, cancellationToken);
         }
 
@@ -51,13 +50,9 @@ namespace Bot_Builder_Simplified_Echo_Bot_V4
             //WITH SAVING STATE WITH ACCESSOR TO 'THEUSERSTATE'
             var botState = await (stepContext.Context.TurnState["DialogBotConversationStateAndUserStateAccessor"] as DialogBotConversationStateAndUserStateAccessor).TheUserProfile.GetAsync(stepContext.Context);
             botState.PassResetEmail = stepContext.Result.ToString();
-            //            await stepContext.Context.SendActivityAsync(MessageFactory.Text($"FOOD WATERFALL STEP 3: I like {botState.Color} {botState.Food} as well! "), cancellationToken);
             //END-WITH SAVING STATE WITH ACCESSOR TO 'THEUSERSTATE'
 
-            //            return await stepContext.EndDialogAsync(null, cancellationToken);
-
             await stepContext.Context.SendActivityAsync(MessageFactory.Text($"Thank you {botState.ITName} "), cancellationToken);
-
             return await stepContext.PromptAsync("passResetBirthDate", new PromptOptions { Prompt = MessageFactory.Text("What is your birthdate? (Format: MM/DD/YYYY)") }, cancellationToken);
         }
 
@@ -71,10 +66,8 @@ namespace Bot_Builder_Simplified_Echo_Bot_V4
             //WITH SAVING STATE WITH ACCESSOR TO 'THEUSERSTATE'
             var botState = await (stepContext.Context.TurnState["DialogBotConversationStateAndUserStateAccessor"] as DialogBotConversationStateAndUserStateAccessor).TheUserProfile.GetAsync(stepContext.Context);
             botState.PassResetBirthDate = stepContext.Result.ToString();
-            //await stepContext.Context.SendActivityAsync(MessageFactory.Text($"FOOD WATERFALL STEP 3: I like {botState.Color} {botState.Food} as well! "), cancellationToken);
             //END-WITH SAVING STATE WITH ACCESSOR TO 'THEUSERSTATE'
 
-            //return await stepContext.EndDialogAsync(null, cancellationToken);
             await stepContext.Context.SendActivityAsync(MessageFactory.Text($"Thank you for confirming. "), cancellationToken);
 
             return await stepContext.PromptAsync("passResetPhoneNumber", new PromptOptions { Prompt = MessageFactory.Text("What is your listed mobile number where we will text you your temporary password. (ie. 555-555-5555) ") }, cancellationToken);
@@ -90,10 +83,7 @@ namespace Bot_Builder_Simplified_Echo_Bot_V4
             //WITH SAVING STATE WITH ACCESSOR TO 'THEUSERSTATE'
             var botState = await (stepContext.Context.TurnState["DialogBotConversationStateAndUserStateAccessor"] as DialogBotConversationStateAndUserStateAccessor).TheUserProfile.GetAsync(stepContext.Context);
             botState.PassResetMobileNumber = stepContext.Result.ToString();
-            //await stepContext.Context.SendActivityAsync(MessageFactory.Text($"FOOD WATERFALL STEP 3: I like {botState.Color} {botState.Food} as well! "), cancellationToken);
             //END-WITH SAVING STATE WITH ACCESSOR TO 'THEUSERSTATE'
-
-            //return await stepContext.EndDialogAsync(null, cancellationToken);
 
             await stepContext.Context.SendActivityAsync(MessageFactory.Text($"Thank you for confirming. "), cancellationToken);
             return await stepContext.PromptAsync("passResetOTPDevice", new PromptOptions { Prompt = MessageFactory.Text("Take a look at your OTP Device.  What is the currently listed code? (ie. ### ###)") }, cancellationToken);
@@ -112,11 +102,9 @@ namespace Bot_Builder_Simplified_Echo_Bot_V4
             botState.PassResetOTPDevice = stepContext.Result.ToString();
             await stepContext.Context.SendActivityAsync(MessageFactory.Text($"Thank you. Your temporary password has been sent to your mobile device. Please immediately log in as it will expire in 4 minutes."), cancellationToken);
             await stepContext.Context.SendActivityAsync(MessageFactory.Text($"We have also sent a notification of your password reset to your work email, your mobile device, and your listed personal email."), cancellationToken);
-
             //END-WITH SAVING STATE WITH ACCESSOR TO 'THEUSERSTATE'
 
             await Task.Delay(4000);
-            
             return await stepContext.EndDialogAsync(null, cancellationToken);
         }
     }
